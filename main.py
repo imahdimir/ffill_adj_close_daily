e7636cc3c7ea230994eddbf8766495e888f67468
imahdimir <imahdimir@gmail.com>
2022-08-05 17:47:05 +0430

    v1

---
+++ main.py
@@ -0,0 +1,62 @@
+##
+
+"""
+
+    """
+
+##
+
+from pathlib import Path
+from pathlib import PurePath
+
+import pandas as pd
+
+wdpn = Path('wds.xlsx')  # all tse working days
+prpn = Path('prices.prq')  # actual adjusted prices dataset
+opn = Path('ffilled.prq')  # output
+
+jdc = 'JDate'
+tic = 'Ticker'
+adc = 'AdjClose'
+
+def main() :
+
+  pass
+
+  ##
+  wdf = pd.read_excel(wdpn)
+  wdf = wdf[jdc]
+
+  ##
+  prdf = pd.read_parquet(prpn)
+  ticks = prdf[tic].drop_duplicates()
+
+  ##
+  df = pd.merge(wdf , ticks , how = 'cross')
+
+  ##
+  cdf = prdf[[jdc , tic , adc]]
+
+  ##
+  df = df.merge(cdf , how = 'outer')
+
+  ##
+  df = df.sort_values(jdc)
+
+  ##
+  gpo = df.groupby(tic)
+  df1 = df.copy()
+  df1[adc] = gpo[adc].ffill()
+
+  ##
+  df1 = df1.dropna()
+
+  ##
+  df1.to_parquet(opn , index = False)
+
+##
+
+
+if __name__ == "__main__" :
+  main()
+  print(f'{PurePath(__file__).name} Done.')
